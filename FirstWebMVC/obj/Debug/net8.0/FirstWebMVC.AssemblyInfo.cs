2d62bf7beb922c6254e3993281f4e7998ed1cb68
tranminh03 <tranvanminh2121051394@gmail.com>
2024-09-06 01:30:11 +0700

    Update_Code

--- FirstWebMVC/obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
+++ FirstWebMVC/obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8c8ecdcbf1e3c405b339661fd99804f1d66698")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+419a4a12e2e5a594a39f2a0b933685c3848e5245")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
